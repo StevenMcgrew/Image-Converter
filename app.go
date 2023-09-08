697b34fba0d3102500f9a405f1c2fd0b70bf2697
StevenMcgrew <stevenmcgrew8224@msn.com>
2023-09-07 21:08:30 -0700

    Loading user preferences

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"os"
@@ -30,6 +31,16 @@ func (a *App) startup(ctx context.Context) {
 Your code below here
 *************************************************
 */
+
+type UserPreferences struct {
+	FolderPaths         []string `json:"folderPaths"`
+	ConvertTo           string   `json:"convertTo"`
+	ResizeWidth         int      `json:"resizeWidth"`
+	ResizeHeight        int      `json:"resizeHeight"`
+	MaintainAspectRatio bool     `json:"maintainAspectRatio"`
+	FillUnusedSpace     bool     `json:"fillUnusedSpace"`
+}
+
 func render(path string, data any) string {
 	if data != nil {
 		var buf bytes.Buffer
@@ -45,6 +56,25 @@ func render(path string, data any) string {
 	}
 }
 
+func getUserPreferences(path string) UserPreferences {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Print(err)
+	}
+	userPreferences := UserPreferences{}
+	json.Unmarshal(bytes, &userPreferences)
+	// figure out how to check if we get some json data,
+	// if no json data, return nil
+	return userPreferences
+}
+
 func (a *App) GetHomePage() string {
-	return render("html/pages/home.html", nil)
+	// get data from store.txt, if any
+	userPref := getUserPreferences("store.json")
+	// render with the stored data, if any
+	if userPref != nil {
+		return render("html/pages/home.html", userPref)
+	} else {
+		fmt.Println("Error at GetHomePage() function")
+	}
 }
